0e49341c647015e53a66c4ed95cd0dd7fec1b140
rahulsingh03 <rsb55426@gmail.com>
2021-06-24 12:34:24 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+api_key = 'YOUR API KEY'
+
+from flask import Flask,render_template
+from form import MyForm
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "4c56141824e5a25073d08e3cc531eb77"
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    form = MyForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        api_url = f"https://cutt.ly/api/api.php?key={api_key}&short={url}"
+
+        try:
+            data = requests.get(api_url).json()["url"]
+        except:
+            pass
+
+        if data["status"] == 7:
+            shortened_url = data["shortLink"]
+            return render_template('index.html',form=form,shortened_url=shortened_url)
+        else:
+            return render_template('index.html',form=form,error='Sorry, Try Again With Appropriate Address!')
+    return render_template('index.html',form=form)
+
+
+if  __name__  == '__main__':
+    app.run(debug=True)
